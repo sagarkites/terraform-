4d04e3f57baa794843df1ae943128991374dfd6d
vidyasagar <33413645+sagarkites@users.noreply.github.com>
2020-10-06 16:42:36 +0530

    Update ec2_provision.tf

--- ec2_provision.tf
+++ ec2_provision.tf
@@ -1,4 +1,3 @@
-
 resource "aws_security_group" "ap_south_sg" {
   name        = "ap_south_sg"
   description = "port22_open_sg"
@@ -29,38 +28,5 @@ resource "aws_instance" "ec2_test" {
     associate_public_ip_address = true
     tags = {
     Name = "Devlopment_PHP"
-  }
-provisioner "remote-exec" {
-    inline = [
-        "sudo yum update -y",
-        "sudo amazon-linux-extras install nginx1.12 -y",
-        "sudo chkconfig nginx on",
-        "sudo chkconfig php-fpm on",
-        "sudo service nginx start",
-        "sudo service php-fpm start",
-        "sudo amazon-linux-extras enable php7.3",
-        "sudo yum install php -y",
-        "sudo yum install -y php-pear",
-        "sudo yum install -y php-cgi",
-        "sudo yum install -y php-common",
-        "sudo yum install -y php-curl",
-        "sudo yum install -y php-mbstring",
-        "sudo yum install -y php-gd",
-        "sudo yum install -y php-mysqlnd",
-        "sudo yum install -y php-gettext",
-        "sudo yum install -y php-bcmath",
-        "sudo yum install -y php-json",
-        "sudo yum install -y php-xml",
-        "sudo yum install -y php-fpm",
-        "sudo install -y php-intl",
-        "sudo yum install -y php-zip"
-    ]
-# Connection 
-connection {
-    type = "ssh"
-    user = "ec2-user"
-    private_key = file("./trouble.pem")
-    host = self.public_ip
-  }
-  }  
+   } 
 }
